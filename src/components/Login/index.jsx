19c70f0d220ff1292457100450ace6d6cb800977
Om21610018 <omkarpenshanwar346@gmail.com>
2024-03-06 13:57:58 +0530

    Frontend Authentication

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+import { ThreeDots } from "react-loader-spinner";
+
+const Login = () => {
+  const [data, setData] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const url = "http://localhost:8080/api/auth";
+      const { data: res } = await axios.post(url, data);
+      setLoading(false);
+      localStorage.setItem("token", res.data);
+      window.location = "/";
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className={`   ${styles.login_container}`}>
+      <div className={`${styles.login_form_container} `}>
+        <div className={`${styles.left}  `}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>Login to Your Account</h1>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            {error && <div className={styles.error_msg}>{error}</div>}
+            <button type="submit" className={styles.green_btn}>
+              {loading ? (
+                <div className=" flex w-full justify-center">
+                  <ThreeDots
+                    visible={true}
+                    height="21"
+                    width="50"
+                    color="#ffffff"
+                    radius="9"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                  />
+                </div>
+              ) : (
+                <div>Sign In</div>
+              )}
+            </button>
+          </form>
+        </div>
+        <div className={styles.right}>
+          <h1>New Here ?</h1>
+          <Link to="/signup">
+            <button type="button" className={styles.white_btn}>
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
